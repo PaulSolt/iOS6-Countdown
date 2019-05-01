231cbf7196e65a012c2a2d9ded20e8a12df9651d
Paul Solt <PaulSolt@gmail.com>
2019-05-01 12:34:55 -0400

    Starting User Interface for the Stop Watch app.

---
+++ iOS6Countdown/CountdownViewController.swift
@@ -0,0 +1,15 @@
+//
+//  CountdownViewController.swift
+//  iOS6Countdown
+//
+//  Created by Paul Solt on 5/1/19.
+//  Copyright © 2019 Lambda, Inc. All rights reserved.
+//
+
+import UIKit
+
+class CountdownViewController: UIViewController {
+    
+    
+    
+}
